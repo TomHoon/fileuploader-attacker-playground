a36f6ac0ed65b012ec7060695eb0bc23b7b3e4ba
donghoonlee <gnsdl9108@naver.com>
2026-08-11 23:17:06 +0900

    fix: upload

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,5 @@
 ./gradlew build -x test
 docker stop daewoo-spring || true
+docker rm daewoo-spring || true
 docker build -t daewoo-spring-image . || true
 docker run -d -p 33000:33000 --network daewoo --name daewoo-spring daewoo-spring-image || true
